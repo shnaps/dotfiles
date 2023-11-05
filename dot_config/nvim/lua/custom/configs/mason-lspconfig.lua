ccc98302eda85c79cbc55fa5a334106bc97cf127
Viachaslau Ravinski <shnapsx@gmail.com>
2023-11-05 09:49:23 -0700

    remove not needed mason config

--- dot_config/nvim/lua/custom/configs/mason-lspconfig.lua
+++
@@ -1,9 +0,0 @@
-local servers = { "html", "jdtls", "cssls", "clangd", "ltex" }
-
-require("mason").setup()
-require("mason-lspconfig").setup {
-  ensure_installed = servers,
-  automatic_installation = true,
-}
-
-return servers
