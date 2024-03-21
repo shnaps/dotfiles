09bf9288d9a10d29bef652cd4555815b058f677b
Viachaslau Ravinski <shnapsx@gmail.com>
2024-03-21 00:08:24 -0400

    Rename and delete dap-ua

--- dot_config/nvim/lua/plugins/nvim-dap-ui.lua
+++ dot_config/nvim/lua/plugins/nvim-dap-ui.lua
@@ -1,15 +1,26 @@
-return { "jay-babu/mason-nvim-dap.nvim" },
-  {
-    "rcarriga/nvim-dap-ui",
-    init = function()
-      -- require("core.utils").load_mappings "dap"
-    end,
-    dependencies = {
-      {
-        "mfussenegger/nvim-dap",
-      },
-    },
-    config = function()
-      require("dapui").setup()
-    end,
-  }
+return {
+	"rcarriga/nvim-dap-ui",
+  -- stylua: ignore
+  keys = {
+    { "<leader>du", function() require("dapui").toggle({ }) end, desc = "Dap UI" },
+    { "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = {"n", "v"} },
+  },
+	opts = {},
+	config = function(_, opts)
+		-- setup dap config by VsCode launch.json file
+		-- require("dap.ext.vscode").load_launchjs()
+		local dap = require("dap")
+		local dapui = require("dapui")
+		dapui.setup(opts)
+		dap.listeners.after.event_initialized["dapui_config"] = function()
+			dapui.open({})
+		end
+		dap.listeners.before.event_terminated["dapui_config"] = function()
+			dapui.close({})
+		end
+		dap.listeners.before.event_exited["dapui_config"] = function()
+			dapui.close({})
+		end
+
+	end,
+}
