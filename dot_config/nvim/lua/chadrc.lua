c99e5ec5ff18722aa86dfe4f17f069e57d5530e4
Viachaslau Ravinski <shnapsx@gmail.com>
2024-05-09 23:54:01 -0400

    update nvim and oh my bash

--- dot_config/nvim/lua/chadrc.lua
+++ dot_config/nvim/lua/chadrc.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 M.ui = {
-	theme = "onedark",
+	theme = "tokyonight",
 	nvdash = {
 		load_on_startup = true,
 
--- dot_config/sway/scripts/executable_swaybg-random.sh
+++ dot_config/sway/scripts/executable_swaybg-random.sh
@@ -9,11 +9,3 @@ while true; do
     kill $OLD_PID
     OLD_PID=$NEXT_PID
 done
-
-00:00:00.730 [DEBUG] [sway/config/output.c:889] spawn_swaybg cmd[0] = swaybg
-00:00:00.730 [DEBUG] [sway/config/output.c:889] spawn_swaybg cmd[1] = -o
-00:00:00.730 [DEBUG] [sway/config/output.c:889] spawn_swaybg cmd[2] = *
-00:00:00.730 [DEBUG] [sway/config/output.c:889] spawn_swaybg cmd[3] = -i
-00:00:00.730 [DEBUG] [sway/config/output.c:889] spawn_swaybg cmd[4] = /home/shnaps/Pictures/wallpaper.jpg
-00:00:00.730 [DEBUG] [sway/config/output.c:889] spawn_swaybg cmd[5] = -m
-00:00:00.730 [DEBUG] [sway/config/output.c:889] spawn_swaybg cmd[6] = fill
