1fd55400ce2fdc25c687b096dd5c42db8eb343e3
Viachaslau Ravinski <shnapsx@gmail.com>
2024-06-05 02:17:12 -0400

    Add couple shortcuts and that's it

--- dot_config/nvim/lua/mappings.lua
+++ dot_config/nvim/lua/mappings.lua
@@ -10,6 +10,23 @@ map("n", "<leader>fm", function()
 	require("conform").format()
 end, { desc = "File Format with conform" })
 
+map('n', '<leader>i', function()
+    -- If we find a floating window, close it.
+    local found_float = false
+    for _, win in ipairs(vim.api.nvim_list_wins()) do
+        if vim.api.nvim_win_get_config(win).relative ~= '' then
+            vim.api.nvim_win_close(win, true)
+            found_float = true
+        end
+    end
+
+    if found_float then
+        return
+    end
+
+    vim.diagnostic.open_float(nil, { focus = false, scope = 'cursor' })
+end, { desc = 'Toggle Diagnostics' })
+
 map("i", "jk", "<ESC>", { desc = "Escape insert mode" })
 map("v", ">", ">gv", { desc = "Indent line" })
 
--- dot_config/nvim/lua/plugins/neogit.lua
+++ dot_config/nvim/lua/plugins/neogit.lua
@@ -1,13 +1,24 @@
-  return{
-    "NeogitOrg/neogit",
-    cmd = "Neogit",
-    dependencies = {
-      "sindrets/diffview.nvim", -- optional - Diff integration
-      "nvim-telescope/telescope.nvim",
-    },
-    config = function()
-      require("neogit").setup()
-      dofile(vim.g.base46_cache .. "git")
-      dofile(vim.g.base46_cache .. "neogit")
-    end,
-  }
+return {
+	"NeogitOrg/neogit",
+	cmd = "Neogit",
+	dependencies = {
+		"sindrets/diffview.nvim", -- optional - Diff integration
+		"nvim-telescope/telescope.nvim",
+	},
+	config = function()
+		-- vim.keymap.set("n", "<leader>gs", neogit.open, { silent = true, noremap = true })
+		--
+		-- vim.keymap.set("n", "<leader>gc", ":Neogit commit<CR>", { silent = true, noremap = true })
+		--
+		-- vim.keymap.set("n", "<leader>gp", ":Neogit pull<CR>", { silent = true, noremap = true })
+		--
+		-- vim.keymap.set("n", "<leader>gP", ":Neogit push<CR>", { silent = true, noremap = true })
+		--
+		-- vim.keymap.set("n", "<leader>gb", ":Telescope git_branches<CR>", { silent = true, noremap = true })
+		--
+		-- vim.keymap.set("n", "<leader>gB", ":G blame<CR>", { silent = true, noremap = true })
+		require("neogit").setup()
+		dofile(vim.g.base46_cache .. "git")
+		dofile(vim.g.base46_cache .. "neogit")
+	end,
+}
