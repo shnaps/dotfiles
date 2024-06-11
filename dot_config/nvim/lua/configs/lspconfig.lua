05520a297418823271b2180c9d36650e9346c821
Viachaslau Ravinski <shnapsx@gmail.com>
2024-06-11 14:44:50 -0400

    Update nvim plugins

--- dot_config/nvim/lua/configs/lspconfig.lua
+++ dot_config/nvim/lua/configs/lspconfig.lua
@@ -25,15 +25,3 @@ for _, lsp in ipairs(servers) do
 		capabilities = capabilities,
 	})
 end
-
--- require("lspconfig").jdtls.setup({
--- 	handlers = {
--- 		-- By assigning an empty function, you can remove the notifications
--- 		-- printed to the cmd
--- 		["$/progress"] = function(_, result, ctx) end,
--- 	},
--- })
---
-vim.diagnostic.config({
-  virtual_text = false,
-})
