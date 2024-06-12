4ad902d4d6cf24ee19b79e68979cbcaacce8b0f3
Viachaslau Ravinski <shnapsx@gmail.com>
2024-06-11 20:42:01 -0400

    Remove custom on_attach

--- dot_config/nvim/lua/configs/lspconfig.lua
+++ dot_config/nvim/lua/configs/lspconfig.lua
@@ -11,17 +11,10 @@ local servers = require("configs.mason")
 
 configs.defaults()
 
-local custom_on_attach = function(client, bufnr)
-	on_attach(client, bufnr)
-	if client.server_capabilities.inlayHintProvider then
-		require("inlay-hints").on_attach(client, bufnr)
-	end
-end
-
 for _, lsp in ipairs(servers) do
 	lspconfig[lsp].setup({
 		on_init = on_init,
-		on_attach = custom_on_attach,
+		on_attach = on_attach,
 		capabilities = capabilities,
 	})
 end
