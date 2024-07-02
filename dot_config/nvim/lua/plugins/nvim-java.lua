14081637639edf7692433754894a8fa9c2268d47
Viachaslau Ravinski <shnapsx@gmail.com>
2024-07-02 17:41:18 -0400

    Update nvim-java

--- dot_config/nvim/lua/plugins/nvim-java.lua
+++ dot_config/nvim/lua/plugins/nvim-java.lua
@@ -1,33 +1,26 @@
 return {
 	"nvim-java/nvim-java",
+	config = false,
 	dependencies = {
-		"nvim-java/lua-async-await",
-		"nvim-java/nvim-java-core",
-		"nvim-java/nvim-java-test",
-		"nvim-java/nvim-java-dap",
-		"nvim-java/nvim-java-refactor",
-		"MunifTanjim/nui.nvim",
-		"neovim/nvim-lspconfig",
-		"mfussenegger/nvim-dap",
 		{
-			"williamboman/mason.nvim",
+			"neovim/nvim-lspconfig",
 			opts = {
-				registries = {
-					"github:nvim-java/mason-registry",
-					"github:mason-org/mason-registry",
+				servers = {
+					jdtls = {
+						-- Your custom jdtls settings goes here
+					},
 				},
-			},
-		},
-		{
-			"williamboman/mason-lspconfig.nvim",
-			opts = {
-				handlers = {
-					["jdtls"] = function()
+				setup = {
+					jdtls = function()
 						require("java").setup({
+							-- Your custom nvim-java configuration goes here
 							handlers = {
 								-- By assigning an empty function, you can remove the notifications
 								-- printed to the cmd
 								["$/progress"] = function(_, result, ctx) end,
+							},
+							jdk = {
+								auto_install = false,
 							},
 						})
 					end,
@@ -35,5 +28,4 @@ return {
 			},
 		},
 	},
-	opts = {},
 }
