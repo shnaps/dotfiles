76c467a6deafcc32be4183cdf4631668f5929180
Viachaslau Ravinski <shnapsx@gmail.com>
2024-10-18 15:09:59 -0400

    Update neovim plugins

--- dot_config/nvim/lua/configs/mason.lua
+++ dot_config/nvim/lua/configs/mason.lua
@@ -1,4 +1,2 @@
 require("mason").setup()
-require("mason-nvim-dap").setup({
-	ensure_installed = { "python" },
-})
+require("mason-nvim-dap").setup({})
--- dot_config/nvim/lua/plugins/illuminate.lua
+++ dot_config/nvim/lua/plugins/illuminate.lua
@@ -1,33 +1,3 @@
 return {
 	"RRethy/vim-illuminate",
-	event = { "CursorHold", "CursorHoldI" },
-	dependencies = "nvim-treesitter",
-	config = function()
-		require("illuminate").configure({
-			under_cursor = true,
-			max_file_lines = nil,
-			delay = 100,
-			providers = {
-				"lsp",
-				"treesitter",
-				"regex",
-			},
-			filetypes_denylist = {
-				"NvimTree",
-				"Trouble",
-				"Outline",
-				"TelescopePrompt",
-				"Empty",
-				"dirvish",
-				"fugitive",
-				"alpha",
-				"packer",
-				"neogitstatus",
-				"spectre_panel",
-				"toggleterm",
-				"DressingSelect",
-				"aerial",
-			},
-		})
-	end,
 }
